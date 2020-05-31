31742f1803c796c56cc0cc65f417317786e250b4
Say-young <seyoung990525@gmail.com>
2020-05-31 21:30:07 +0900

    autoTransaction 구현 오나료

--- server/models/capital.js
+++ server/models/capital.js
@@ -3,32 +3,8 @@ const userTable = 'user';
 const autoTransTable = 'autoTransfer';
 const noticeTable = 'notice';
 
-/*
-형태 참고
-"status": 200,
-    "success": true,
-    "message": "자동이체 정보 출력 성공",
-    "data" : [
-        {
-        "date" : "2020년 05월 16일 토요일", notice - date
-
-        "userName" : "이희수", user - name
-        "userAccount" : "43553-**-***902", user - account
-
-        "otherName" : "김형호", user - name2
-        "otherAccount" : "016-21-0***", user - account2
-
-        "flag" : 0, autoTransfer - flag
-        "dueDate" : "2020.02.04", autoTransfer - dueDate 
-        "content" : "월세" autoTransfer - content
-    }
-*/
-
 const capital = {
     autoTransfer: async (userIdx) => {
-
-        //const query = `SELECT * FROM ${userTable} WHERE userIdx = ${userIdx}`;
-
 
         const getUserQuery = `SELECT * FROM ${userTable} WHERE userIdx = ${userIdx}`;
         const getNoticeQuery = `SELECT * FROM ${noticeTable} WHERE userIdx = ?`;
@@ -71,22 +47,8 @@ const capital = {
                 newResult.push(addResult);
             }
         }
-
-
+        
         return newResult;
-
-/*
-        //
-        //이렇게 하면 date, name, account 나옴
-        const query = `SELECT * FROM ${userTable} natural join ${noticeTable} WHERE userIdx = ${userIdx}`;
-        //otherUser의 정보를 어떻게 이 정보들이랑 합쳐서 한번에 출력할지 방법 고민중...
-        try {
-            const result = await pool.queryParamArr(query);
-            return result;
-        } catch (err) {
-            console.log('자동 이체 내역 조회 실패 : ', err);
-            throw err;
-        }*/
     }
 }
 
